fed53b7f00d4d713b6388b38dffa854f61044f51
Maggie Danger <magnus@ef.gy>
2020-12-07 10:34:08 +0000

    introduce lazy pointers that can be validated like views, and use them for the tilesets

--- include/pokemon-randomiser/tileset.h
+++ include/pokemon-randomiser/tileset.h
@@ -11,7 +11,9 @@ class bgry : gameboy::rom::view<B, W> {
  public:
   using view = gameboy::rom::view<B, W>;
   using pointer = typename view::pointer;
+  using lazy = typename view::lazy;
   using subviews = typename view::subviews;
+  using lazies = typename view::lazies;
 
   /* The BGRY section used Bulbapedia extensively for its research content.
    *
@@ -19,50 +21,61 @@ class bgry : gameboy::rom::view<B, W> {
    * https://bulbapedia.bulbagarden.net/wiki/User:Tiddlywinks/Map_header_data_structure_in_Generation_I
    */
 
+ protected:
+  static constexpr pointer start{0xc7be};
+  static constexpr pointer end{0xc8dd};
+
  public:
   bgry(view v, uint8_t pID)
-      : view{v.from(headersStart + id * headerSize).length(headerSize)},
+      : view{v.from(start + pID * headerSize).length(headerSize)},
         id(pID),
-        bank{view::start().is(gameboy::rom::dt_byte)},
-        blocks{view::after(bank)
-                   .is(gameboy::rom::dt_word)
+        bank_{view::start().is(gameboy::rom::dt_rom_bank)},
+        blocks_{view::after(bank_)
+                    .is(gameboy::rom::dt_rom_offset)
+                    .expect(gameboy::rom::e_little_endian)},
+        tiles_{view::after(blocks_)
+                   .is(gameboy::rom::dt_rom_offset)
                    .expect(gameboy::rom::e_little_endian)},
-        tiles{view::after(blocks)
-                  .is(gameboy::rom::dt_word)
-                  .expect(gameboy::rom::e_little_endian)},
-        collision{view::after(tiles)
-                      .is(gameboy::rom::dt_word)
-                      .expect(gameboy::rom::e_little_endian)},
-        talkOver{view::after(collision).length(3).is(gameboy::rom::dt_bytes)},
-        grass{view::after(talkOver).is(gameboy::rom::dt_byte)},
-        animation{view::after(grass).is(gameboy::rom::dt_byte)},
-        subviews_{&bank,     &blocks, &tiles,    &collision,
-                  &talkOver, &grass,  &animation} {}
+        collision_{view::after(tiles_)
+                       .is(gameboy::rom::dt_rom_offset)
+                       .expect(gameboy::rom::e_little_endian)},
+        talkOver_{view::after(collision_).length(3).is(gameboy::rom::dt_bytes)},
+        grass_{view::after(talkOver_).is(gameboy::rom::dt_byte)},
+        animation_{view::after(grass_).is(gameboy::rom::dt_byte)},
+        blocks{bank_, blocks_},
+        tiles{bank_, tiles_},
+        collision{bank_, tiles_},
+        subviews_{&bank_,     &blocks_, &tiles_,    &collision_,
+                  &talkOver_, &grass_,  &animation_},
+        lazies_{&blocks, &tiles, &collision} {}
 
   const uint8_t id;
 
-  view bank;
-  view blocks;
-  view tiles;
-  view collision;
-  view talkOver;
-  view grass;
-  view animation;
-
   operator bool(void) const {
-    return id < headers() && bool(view(*this)) && view::check(subviews_);
+    return bool(view(*this)) && view::check(subviews_) && view::check(lazies_);
   }
 
  protected:
-  const subviews subviews_;
+  view bank_;
+  view blocks_;
+  view tiles_;
+  view collision_;
+  view talkOver_;
+  view grass_;
+  view animation_;
 
-  static const long headersStart = 0xc7be;
-  static const long headersEnd = 0xc8dd;
+ public:
+  lazy blocks;
+  lazy tiles;
+  lazy collision;
+
+ protected:
+  const subviews subviews_;
+  const lazies lazies_;
+
   static const long headerSize = 12;
 
-  static const long headers(void) {
-    return (headersEnd - headersStart + 1) / headerSize;
-  }
+  static const long headers(void) { return (end - start + 1) / headerSize; }
 };
 
 }  // namespace tileset
